19ea45b528392286690ab0d0ac98d15b74336960
Manvir Kaur <Manvir Kaur@LAPTOP-5ATUSNJJ>
2019-07-26 10:04:52 +0530

    2nd commit

--- src/git/sample.java
+++ src/git/sample.java
@@ -5,6 +5,8 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("hello");
+       sample1 s=new sample1();
+       s.hello();
 	}
 
 }
---
+++ src/git/sample1.java
@@ -0,0 +1,9 @@
+package git;
+
+public class sample1 {
+	public void hello()
+	{
+    System.out.println("hello1");
+	}
+	
+}
